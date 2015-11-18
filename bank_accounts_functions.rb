4531a23dc92f6bf11c11739094d1bac40df6bb1f
User <user@Users-MBP-3.home>
2015-11-18 21:27:33 +0000

    Array and Hashes Homework commit 1

---
+++ bank_accounts_functions.rb
@@ -0,0 +1,98 @@
+ACCOUNTS = [
+  {
+    holder_name: "Jay",
+    amount: 55,
+    type: "business"
+  },
+  {
+    holder_name: "Rick",
+    amount: 1,
+    type: "personal"
+  },
+  {
+    holder_name: "Keith",
+    amount: 500,
+    type: "business"
+  },
+  {
+    holder_name: "Valerie",
+    amount: 100,
+    type: "personal"
+  },
+  {
+    holder_name: "Michael",
+    amount: 5,
+    type: "personal"
+  },
+  {
+    holder_name: "Kate",
+    amount: 2000,
+    type: "business"
+  },
+  {
+    holder_name: "Tony",
+    amount: 150,
+    type: "personal"
+  },
+  {
+    holder_name: "Sandy",
+    amount: 4500,
+    type: "business"
+  },
+]
+#Problem 1_________________________
+    # def number_of_bank_accounts()
+    #   ACCOUNTS.length
+    # end
+#Problem 2_________________________
+    #def first_bank_account_holder()
+    #ACCOUNTS[0][:holder_name]
+    # end  
+#Problem 3_________________________
+    # def print_owner_names()
+    #   for accounts in ACCOUNTS
+    #      puts "#{accounts[:holder_name]}"
+    #   end
+    # end
+#Problem 4_______________________________
+    # def total_cash_in_bank
+    #   total = 0
+    #   for account in ACCOUNTS
+    #     total = total + account[:amount]
+    #   end  
+    #     return total
+    # end  
+#Problem 5_______________________________
+    #def last_bank_account_holder()
+    #  ACCOUNTS.last[:holder_name]
+    #end
+
+ #Problem 6______________________________
+    # def average_cash_in_account
+    #     total = 0
+    #     for account in ACCOUNTS
+    #     total = total + account[:amount]
+    #     end  
+    #        average = total/ACCOUNTS.length
+    #        return average
+    # end  
+#Problem 7_______________________________
+def business_total_cash_in_bank
+  total_business_amount = 0
+    for account in ACCOUNTS
+      if account[:type] == "business"
+      total_business_amount = total_business_amount + account[:amount]
+    end
+    end
+    total_business_amount
+  end
+
+
+#Problem 8____________________________________]
+
+ 
+
+
+
+
+
---
+++ bank_accounts_runner.rb
@@ -0,0 +1,49 @@
+require_relative('./bank_accounts_functions')
+
+##number of bank accounts
+  # bank_accounts_count = number_of_bank_accounts()
+  # puts "number of accounts #{bank_accounts_count}"
+  # puts "\n"
+
+###first account owner
+  # first_bank_owner = first_bank_account_holder()
+  # puts "first bank account owner is #{first_bank_owner}"
+  # puts "\n"
+
+###print to screen name of all bank account owners
+    # puts "name of bank acccount owners"
+    # print_owner_names()
+    # puts "\n"
+
+###total cash in bank
+  # total_cash = total_cash_in_bank
+  # puts "total in bank #{total_cash}"
+  # puts "\n"
+
+###last account owner
+
+  # last_bank_owner = last_bank_account_holder()
+  # puts "last bank account owner is #{last_bank_owner}"
+  # puts "\n"
+
+###average bank account value
+  # average_cash = average_cash_in_account
+  #   puts "Average amount in bank is £#{average_cash}"
+  #   puts "\n"
+
+###total cash in business accounts
+
+business_total_cash = business_total_cash_in_bank
+puts "#{business_total_cash}"
+puts "\n"
+
+###holder of largest bank account
+
+ # holder_of_largest_total = largest_total
+ # puts "#{holder_of_largest_total}"
+ # puts "\n"
+
+###holder of largest personal account
+
+###any other functionality you want to add.
+
