0123005e47d13a7570d9db3ce38da8b41d79661f
User <user@Users-MacBook-Pro-4.local>
2015-11-19 08:45:22 +0000

    Tided up the code

--- bank_accounts_functions.rb
+++ bank_accounts_functions.rb
@@ -77,22 +77,22 @@ ACCOUNTS = [
     #        return average
     # end  
 #Problem 7_______________________________
-# def business_total_cash_in_bank
-#   total_business_amount = 0
-#     for account in ACCOUNTS
-#       if account[:type] == "business"
-#       total_business_amount = total_business_amount + account[:amount]
-#     end
-#     end
-#     total_business_amount
-#   end
+    # def business_total_cash_in_bank
+    #   total_business_amount = 0
+    #     for account in ACCOUNTS
+    #       if account[:type] == "business"
+    #       total_business_amount = total_business_amount + account[:amount]
+    #     end
+    #     end
+    #     total_business_amount
+    #   end
 
 
 #Problem 8____________________________________]
 
- # def largest_total
-  # rich_man = ACCOUNTS.max_by{|account| account[:amount]}
-  # rich_man[:holder_name]
+    # def largest_total
+    # rich_man = ACCOUNTS.max_by{|account| account[:amount]}
+    # rich_man[:holder_name]
 
   #    # select{|account| account[:type] == "personal"}
   #    #What to just return the name
